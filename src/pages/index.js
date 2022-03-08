8a781439d4e1d0cd28870afdba6a846fd0fd0388
Sai Phaltankar <saiphaltankar@gmail.com>
2022-03-08 12:10:28 +0530

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Sai Phaltankar" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <AboutSection sectionId="about" heading="About Me" />
